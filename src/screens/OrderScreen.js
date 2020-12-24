75b8633afa19764fd02c83b73795848d868f1ab9
thanghoangngoc <thanghn@ominext.com>
2020-12-24 14:20:41 +0700

    show don hang

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,5 +1,7 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
-import { Link, useRouteMatch, useParams, useHistory } from "react-router-dom";
+import { useRouteMatch, useHistory } from "react-router-dom";
+import Constant from '../../src/constant';
 
 class OrderScreen extends Component {
 
@@ -11,14 +13,57 @@ class OrderScreen extends Component {
     }
 
     componentDidMount() {
-        
+        this.getOrders();
+    }
+
+    getOrders = async () => {
+        try {
+            const user = await localStorage.getItem('user');
+            const userPar = JSON.parse(user);
+            const res = await Axios.get(`${Constant.BASE_URL}/order/show`, {
+                headers: {
+                    'Authorization': `Bearer ${userPar && userPar.access_token}`
+                }
+            });
+            console.log('getOrders:', res);
+            if (res.data.status != 200) {
+                alert(res.data.message);
+                return;
+            }
+            this.setState({
+                data: res.data.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    renderListOrder = () => {
+        const { data } = this.state;
+        return (
+            data.map((e, i) => {
+                return (
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginTop: 8,
+                        backgroundColor: '#46A049',
+                        width: 500
+                    }}>
+                        <span>Mã đơn hàng: {e.id}</span>
+                        <span>Ngày tạo: {e.date}</span>
+                        <span style={{ color: '#BF081F' }}>Tổng tiền: {e.amount} VNĐ</span>
+                    </div>
+                )
+            })
+        )
     }
 
     render() {
         return (
             <div>
                 <h1>Đơn hàng</h1>
-
+                {this.renderListOrder()}
             </div>
         );
     }
