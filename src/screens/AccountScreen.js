e7d59767815fba6a6fc62b31d341268b788034c8
thanghoangngoc <thanghn@ominext.com>
2020-12-28 16:14:17 +0700

    ui acc

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,45 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import Constant from '../constant';
 
 class AccountScreen extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             name: '',
             phone: '',
             address: '',
-            avatar: ''
+            avatar: null
         };
+    }
+
+    componentDidMount() {
+        this.showUser()
+    }
+
+    showUser = async () => {
+        try {
+            const user = await localStorage.getItem('user');
+            const userPar = JSON.parse(user);
+            const res = await Axios.get(`${Constant.BASE_URL}/user/show`, {
+                headers: {
+                    'Authorization': `Bearer ${userPar && userPar.access_token}`
+                }
+            });
+            console.log(res);
+            if (res.data.status != 200) {
+                return;
+            }
+            this.setState({
+                name: res.data.data.name,
+                phone: res.data.data.phone,
+                address: res.data.data.address
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChangeName = (e) => {
@@ -24,8 +55,41 @@ class AccountScreen extends Component {
     }
 
 
-    onFileChange = () => {
+    onFileChange = (e) => {
+        console.log(e);
 
+        this.setState({
+            avatar: e.target.files[0]
+        })
+    }
+
+    onSave = async () => {
+        if (this.state.avatar !== null) {
+            try {
+                const user = await localStorage.getItem('user');
+                const userPar = JSON.parse(user);
+                
+                let formData = new FormData();
+                formData.append("name", this.state.name);
+                formData.append("phone", this.state.phone);
+                formData.append("address", this.state.address)
+                formData.append('avatar', this.state.avatar);
+
+                const res = await Axios.post(`${Constant.BASE_URL}/user/upload`,
+                    formData,
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${userPar && userPar.access_token}`,
+                            "Content-type": "multipart/form-data",
+                        },
+                    }
+                )
+
+                console.log(res);
+            } catch (error) {
+                console.log("loi:",error);
+            }
+        }
     }
 
     render() {
@@ -66,6 +130,12 @@ class AccountScreen extends Component {
                     type="file"
                     style={{ width: 200, marginTop: 10 }}
                     onChange={this.onFileChange} />
+
+                <div
+                    onClick={this.onSave}
+                    style={{ width: 100, height: 30, backgroundColor: 'green', padding: 4, marginTop: 10 }}>
+                    <span>Lưu</span>
+                </div>
             </div>
         );
     }
